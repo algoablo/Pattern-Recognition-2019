a528c1fc5aec4c01b8cfb0bc9f4fe9eb0e7d0599
Arbër Kuçi <arber.kuci2@gmail.com>
2019-04-01 00:06:50 +0200

    Plot the accuracy for training & validation data with respect to the epoches for the best found classifier.

--- mlp.py
+++ mlp.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import KFold
-from datetime import datetime
+import matplotlib.pyplot as plt
 
 
 def read_data(filename):
@@ -10,7 +10,6 @@ def read_data(filename):
         reader = csv.reader(f)
         data = list(reader)
     matrix = np.array(data, dtype=int)
-    # separate labels from samples
     samples = matrix[:, 1:]
     labels = matrix[:, 0]
     return labels, samples
@@ -21,7 +20,6 @@ def main():
     test_filename = 'mnist_test.csv'
 
     train_labels, train_samples = read_data(train_filename)
-    start_total = datetime.now()
     best_classifier = None
     max_accuracy = 0
     for iter in range(5):
@@ -59,8 +57,50 @@ def main():
                     best_classifier = classifier
     print('Best accuracy found with the MLP with the following parameters: {params}'.format(params=best_classifier))
 
-    end = datetime.now()
-    print('Total runtime: {duration}'.format(duration=end - start_total))
+    """Get the accuracy on the testset with the best parameters found during the training phase of the MLP"""
+    test_labels, test_samples = read_data(test_filename)
+    best_classifier.fit(train_samples, train_labels)
+    score = best_classifier.score(test_samples, test_labels)
+    print('Accuracy = {acc}'.format(acc=score))
+
+    """Plot the accuracy on train & validation dataset for the best classifier."""
+    neurons = best_classifier.hidden_layer_sizes[0]
+    learning_rate = best_classifier.learning_rate_init
+
+    k_folds = KFold(n_splits=4)
+
+    for train, validation in k_folds.split(train_samples):
+        accuracies_training_dataset = []
+        accuracies_validation_dataset = []
+        epoches = []
+
+        training_samples = train_samples[train]
+        training_labels = train_labels[train]
+
+        test_samples = train_samples[validation]
+        test_labels = train_labels[validation]
+
+        for iter in range(5):
+            iteration = (iter + 1) * 200
+
+            best_classifier = MLPClassifier(activation='logistic', solver='sgd', hidden_layer_sizes=(neurons,),
+                                            max_iter=iteration, learning_rate='constant',
+                                            learning_rate_init=learning_rate)
+
+            best_classifier.fit(training_samples, training_labels)
+
+            score = best_classifier.score(test_samples, test_labels)
+            epoches.append(iteration)
+            accuracies_training_dataset.append(score)
+
+            score = best_classifier.score(training_samples, training_labels)
+            accuracies_validation_dataset.append(score)
+
+        plt.plot(epoches, accuracies_training_dataset, color='green')
+        plt.plot(epoches, accuracies_validation_dataset, color='orange')
+        plt.xlabel('Epoche')
+        plt.ylabel('Accuracy')
+        plt.show()
 
 
 if __name__ == '__main__':
